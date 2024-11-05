651d3f861bc26da8bc22f22c229e776cfbc08170
web-ghoul <mahmoudaboraya2021@gmail.com>
2024-11-05 03:51:04 +0200

    first commit

--- api/index.php
+++ api/index.php
@@ -1,1 +1,10 @@
-<?php require __DIR__ . '/../public/index.php';
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$kernel = $app->make("Illuminate\Contracts\Http\Kernel::class");
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+$response->send();
+$kernel->terminate($request, $response);
